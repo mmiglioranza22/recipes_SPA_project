eab79781fd1eebf4c7da3df6340449808c7a8029
Manuel Miglioranza <mmiglioranza22@gmail.com>
2021-07-08 19:20:35 -0300

    getRecipeById controller working, errors catched, not passed with next. Minos modif constants, number=100

--- api/src/constants.js
+++ api/src/constants.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { API_KEY_1, API_KEY_2, API_KEY_3, API_KEY_4, API_KEY_5 } = process.env;
 const BASE_URL = 'https://api.spoonacular.com/recipes/';
-const URL_FLAGS = 'number=9&addRecipeInformation=true';
+const URL_FLAGS = 'number=100&addRecipeInformation=true';
 
 module.exports = {
 	BASE_URL,
--- api/src/controllers/recipe.js
+++ api/src/controllers/recipe.js
@@ -3,9 +3,9 @@ const axios = require('axios').default;
 const { Diet, Recipe } = require('../db.js'); // se importa de db.js porque ahi es donde se leen por fs.readdirSync los archivos de models y se exportan por sequelize.models
 const { BASE_URL, URL_FLAGS, API_KEY_1, API_KEY_2, API_KEY_3, API_KEY_4, API_KEY_5 } = require('../constants');
 
-async function getAllRecipes (req, res, next){
+async function getAllRecipes(req, res, next) {
 	let { name } = req.query;
-	let recipesDB = []; 
+	let recipesDB = [];
 	if (req.query.name) {
 		Recipe.findAll({
 			where: {
@@ -25,7 +25,7 @@ async function getAllRecipes (req, res, next){
 			}
 			let apiResponse = [];
 			promiseApi.data.results.forEach(recipe => {
-				let { image, title, diets, vegetarian, vegan, glutenFree, dairyFree  } = recipe; // por interpretacion, faltaria dishTypes(array)
+				let { image, title, diets, vegetarian, vegan, glutenFree, dairyFree } = recipe; // por interpretacion, faltaria dishTypes(array)
 				let result = { image, title, diets, vegetarian, vegan, glutenFree, dairyFree };
 				apiResponse.push(result);
 			});
@@ -40,32 +40,51 @@ async function getAllRecipes (req, res, next){
 
 async function getRecipeById(req, res, next) {
 	let { idReceta } = req.params;
-	if (idReceta.length === 36) {
+	if (idReceta && idReceta.length === 36) {
 		try {
 			let result = await Recipe.findOne({
 				where: {
 					id: idReceta
 				}
 			});
-			return res.json(result)
-		} catch (err) {
+			if (result) {
+				return res.json(result)
+			} else {
+				return res.status(404).json({
+					status: 404,
+					code: 0,
+					message: "We're so sorry, something went wrong. If this error persists, please contact us.",
+					link: null,
+					infoDev: 'Id requested not found in database',
+				});
+			}
+		} catch (err) { // este catch estaria de mas, ya estoy controlando si el findOne me devuelve [] en el else
 			next(err)
-		}
+		};
 	};
-	if (typeof parseInt(idReceta) === 'number') {
+	if (/^[^A-Za-z]+$/i.test(idReceta)) { // regex para testear que idReceta sea unicamente un numero, sin caracteres en ningun lado
+		// parseInt no funcionaba si habia caracteres despues del numero!
+		let promiseApi;
 		try {
-			let promiseApi = await axios.get(`${BASE_URL}${idReceta}/information?apiKey=${API_KEY_2}`);
-			if (promiseApi.data.message) res.status(404).send(promiseApi.data);
+			promiseApi = await axios.get(`${BASE_URL}${idReceta}/information?apiKey=${API_KEY_2}`);
 			let { image, title, diets, vegetarian, vegan, glutenFree, dairyFree, summary, spoonacularScore, healthScore, instructions } = promiseApi.data;
 			let result = { image, title, diets, vegetarian, vegan, glutenFree, dairyFree, summary, spoonacularScore, healthScore, instructions };
 			return res.json(result)
 		} catch (err) {
-			next(err);
+			console.log('DEMASIADOS NUMEROOOOOOOOOS---------------')
+			//next(err) // pasa el error y lo consologea al final desde el endware de app.js, pero lo pasa de esa forma concreta
+			// si quiero manejarlo aca, no tengo que usar next
+			return res.status(404).json({
+				...err.response.data, infoDev: 'Id requested not found in API'})
 		}
-	}
+	};
+
+	// res.status(404).send('Invalid id: Recipe Id must be a number') no haria falta, los cases estan cubiertos
+
+	
 };
 
-async function createRecipe (req, res, next) {
+async function createRecipe(req, res, next) {
 	try {
 		let { name, summary, score, healthScore, instructions, dietTypes } = req.body;
 		let [recipe, created] = await Recipe.findOrCreate({
